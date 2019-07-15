2ed7afe134a998e0195e410bdb59d1bad1a97828
alan1tavares <alan1tavares@gmail.com>
2019-07-15 11:07:12 -0300

    Front-end   Começado a estruturar o html.

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,9 +1,15 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, 
+      window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
+
+// TODO Definir a atura e lagura do visualizador 3D e passar essas dimensões
+// para o render.
+const visualizador3d = document.querySelector('#visualizador3d');
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+visualizador3d.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
