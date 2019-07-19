e83301f94422a0f92657828a463fd64a83fdbaca
alan1tavares <alan1tavares@gmail.com>
2019-07-19 10:55:34 -0300

    Front-end    Refatorado o código do cubo. Em vez de passar o dom que o threejs gera    página é passado o canvas diretamente para o threejs.    Adicionado um folha de estilo. Iniciado a montagem da página com css grid    Layout.    Mudado o html.

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,30 +1,38 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, 
-      window.innerWidth / window.innerHeight, 0.1, 1000);
-
-var renderer = new THREE.WebGLRenderer();
-
 // TODO Definir a atura e lagura do visualizador 3D e passar essas dimensões
-// para o render.
-const visualizador3d = document.querySelector('#visualizador3d');
+// para o render.'
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-visualizador3d.appendChild(renderer.domElement);
+function cuboRodando() {
+      const canvas = document.querySelector('#visualizador3d');
+      var renderer = new THREE.WebGLRenderer({ canvas });
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
-camera.position.z = 5;
+      const fieldOfView = 75;
+      const aspect = 2;
+      const near = 0.1;
+      const far = 5;
+      const camera = new THREE.PerspectiveCamera(fieldOfView, aspect, near, far);
+      camera.position.z = 2;
 
-var animate = function () {
-   requestAnimationFrame(animate);
+      const scene = new THREE.Scene();
 
-   cube.rotation.x += 0.01;
-   cube.rotation.y += 0.01;
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
 
-   renderer.render(scene, camera);
-};
+      // renderer.render(scene, camera);
+      // renderer.setSize(width, height);
 
-animate();
+      var animate = function () {
+            requestAnimationFrame(animate);
+
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+
+            renderer.render(scene, camera);
+      };
+
+      animate();
+}
+
+cuboRodando();
